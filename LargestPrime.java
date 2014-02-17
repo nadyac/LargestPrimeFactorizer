87b9262ca3b1f9ee6b4c5a889776190ac6b89b18
Nadya <nadyacpena@gmail.com>
2014-02-16 16:11:23 -0800

    Updated Finder of Largest Prime
    Still a work in progress. It now parses the given number and checks if
    it's a multiple of 2 and 3 by using the divisibility rules.

--- LargestPrime.java
+++ LargestPrime.java
@@ -24,11 +24,24 @@ public class LargestPrime{
 			/*create int array to hold all of the digits*/
 			int[] numbers= new int[length];
 
-
+			/*populate the int array with the individual digits*/
 			for(int x=0;x<length;x++){
 				numbers[x]= Integer.parseInt(number.substring(x,x+1));
-				System.out.println(numbers[x]);
+				//System.out.println(numbers[x]);
 			}
+
+			/*Check divisibility by two - (see) if the last digit is even*/
+			//System.out.println("the last digit is: " + numbers[length-1]);
+			if((numbers[length-1])%2 ==0){
+				System.out.println("This number is a multiple of 2");
+			}else{
+				System.out.println("This number is NOT a multiple of 2");
+			}
+			/*Check divisibility by 3 - check if the sum of all digits is multiple of 3*/
+
+			/*Check divisibility by 7 - try division, no rule other rules apply*/
+
+			/*If all the checks fail, brute force it by checking divisibility from (11,sqty(n)]*/
 		}catch(Exception e){
 			System.out.println("Error");
 		}	
